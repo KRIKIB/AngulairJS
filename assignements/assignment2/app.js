b4879f50974b4738606c7d1fffaac5b3020f8b41
adil <you@example.com>
2018-05-12 15:37:31 +0200

    final

--- assignements/assignment2/app.js
+++ assignements/assignment2/app.js
@@ -12,34 +12,38 @@ function ToBuyController(ListService)
 {
 
  var ctr1 = this ;
+ var itemsBought = [];
 ctr1.items =  ListService.getItemsToBuy() ;
 
  ctr1.remove =  function ( index ) {
-
+  ctr1.messageLeft
       ListService.addItem(index );
    ListService.removItem( index  ) ;
 if ( !ctr1.items.length  ) {
-  ctr1.message = "Everything is bought!";
-}
-
- } ;
+    ctr1.message = "Everything is bought!";
  }
+
+}
+}
 function AlreadyBoughtController( ListService)
 {
 
 var ctr2 = this ;
  ctr2.items =  ListService.getItemsBought() ;
- if ( ctr2.items.length ) {
+
    ctr2.message = "Nothing bought yet.";
- }
+
+
 
 }
 //
 function ListService ()
 {
  var service = this ;
- var itemsToBuy = [  { name: "cookies", quantity: 10 } ,{ name: "cookies", quantity: 10 },
-{ name: "cookies", quantity: 10 }];
+ var msgLeft ;
+ var msgRight = "Nothing bought yet."
+ var itemsToBuy = [  { name: "compot", quantity: 10 } ,{ name: "cookies", quantity: 10 },
+{ name: "chocolat", quantity: 10 }];
  var itemsBought = [];
  service.addItem = function (index) {
   var item =  itemsToBuy[index];
@@ -56,5 +60,10 @@ service.removItem  = function( itemIndex ) {
  {
   return   itemsBought ;
  };
+ service.setMassage = function functionName() {
+   if (!itemsBought.length) {
+
+   }
+ }
 }
 })();
